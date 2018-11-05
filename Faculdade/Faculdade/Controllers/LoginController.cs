52e0d029269da93109d7dabb8dbed087c7325e37
Cuchoa <44787065+Cuchoa@users.noreply.github.com>
2018-11-05 19:21:40 -0200

    Add files via upload

---
+++ Faculdade/Faculdade/Controllers/LoginController.cs
@@ -0,0 +1,45 @@
+﻿using Faculdade.Models;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace Faculdade.Controllers
+{
+    [AllowAnonymous]
+    public class LoginController : Controller
+    {
+        private rentacarEntities rentacar = new rentacarEntities();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Users user)
+        {
+            if (!ModelState.IsValid)
+                return View();
+            user.password = string.Join("", SHA1CryptoServiceProvider.Create().ComputeHash(Encoding.UTF8.GetBytes(user.password)).SelectMany(x => x.ToString("x2")));
+            var doLogin = rentacar.Users.Where(model => model.register == user.register && model.password == user.password).FirstOrDefault();
+
+            if (doLogin != null)
+            {
+                FormsAuthentication.SetAuthCookie(doLogin.name, false);
+
+                if (Request.QueryString["ReturnUrl"] == null)
+                    return RedirectToAction("", "Home");
+                else
+                    return RedirectToAction(Request.QueryString["ReturnUrl"].ToString());
+            }
+            else
+            {
+                ModelState.AddModelError("", "Usuário e ou senha inválido!");
+            }
+
+            return View(user);
+        }
+    }
+}
